7781dcf6e79d1eff2fb49d5d703e00d6f8392d70
Manoj <101968434+manoj9885762348@users.noreply.github.com>
2022-06-25 12:34:20 +0530

    Update jenkins.sh

--- jenkins file/jenkins.sh
+++ jenkins file/jenkins.sh
@@ -1,8 +1,9 @@
 node{
-stages("Git Clone"){
-    git clone https://github.com/spring-projects/spring-petclinic.git
-cd spring-petclinic
-./mvnw package
-java -jar target/*.jar
-  }
+  stages{
+      stage("Git Clone"){
+      git clone 'https://github.com/manoj9885762348/spring-petclinic.git'
+      cd spring-petclinic
+      ./mvnw package
+       java -jar target/*.jar
+    }
 }
